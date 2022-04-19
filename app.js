e1d4beb70064c130f807d379009b165d44cc3478
Anna Tuban <mshannahstarling@gmail.com>
2022-04-19 19:02:35 +0300

    feat: add routes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { connect } = require('mongoose');
+const router = require('./routes');
 const { PORT } = require('./utils/constants');
 
 const app = express();
+app.use(router);
 
 const start = async () => {
   await connect('mongodb://localhost:27017/moviesdb');
