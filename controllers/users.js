19e50cd8891eeb8de1cfa4ae856fdbb9a70a1cbf
Anna Tuban <mshannahstarling@gmail.com>
2022-04-19 19:29:36 +0300

    feat: add user controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,34 @@
-const getUserInfo = () => {};
-const updateUser = () => {};
+const User = require('../models/user');
+
+const getUserInfo = async (req, res, next) => {
+  try {
+    const userInfo = await User.findById(req.user._id);
+    return userInfo
+      ? res.status(200).send(userInfo)
+      : next(new Error('Пользователь не найден'));
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name, email },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    return user
+      ? res.status(200).send({ name: user.name, email: user.email })
+      : next(new Error('Пользователь не найден'));
+  } catch (error) {
+    return next(error);
+  }
+};
 
 module.exports = {
   getUserInfo,
