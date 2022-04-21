53ee877f6bb1567bb24822f0e3339b9c474ee49c
Anna Tuban <mshannahstarling@gmail.com>
2022-04-21 23:40:35 +0300

    fix: auth protect all routes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ const { auth } = require('../middlewars/auth');
 const { handleNotFound } = require('./notFound');
 
 router.use('/api', authRoutes);
-router.use('/api/users', auth, userRouter);
-router.use('/api/movies', auth, movieRouter);
+router.use(auth);
+router.use('/api/users', userRouter);
+router.use('/api/movies', movieRouter);
 router.use('/*', handleNotFound);
 
 module.exports = router;
