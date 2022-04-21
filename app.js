021d591d1166056f058c4597914494bb997d9a27
Anna Tuban <mshannahstarling@gmail.com>
2022-04-21 19:48:33 +0300

    feat security: add helmet CSP

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const { connect } = require('mongoose');
 const { errorHandler } = require('./middlewars/errorHandler');
 const { requestsLogger, errorsLogger } = require('./middlewars/logger');
@@ -11,6 +12,7 @@ const { PORT, DATA_BASE, CORS_OPTIONS, LIMITER } = require('./utils/constants');
 const app = express();
 app.use(cors(CORS_OPTIONS));
 app.options('*', cors());
+app.use(helmet()); // Content-Security-Policy
 app.use(LIMITER);
 app.use(cookieParser());
 app.use(express.json());
